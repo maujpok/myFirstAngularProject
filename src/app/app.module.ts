9dbbf2952028b8aecfc5cc8bb2c51d8bbe271eb6
Mauricio Portillo <Mauricio Portillo@VKMNOTE-070>
2022-01-18 12:17:40 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {firstComponentComponent} from './components/firstComponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    firstComponentComponent
   ],
   imports: [
     BrowserModule
