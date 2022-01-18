d93e23a2115c61525de1c08de19069172531f22c
Mauricio Portillo <Mauricio Portillo@VKMNOTE-070>
2022-01-18 13:03:29 -0300

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {firstComponentComponent} from './components/firstComponent.component';
+import {firstComponentComponent} from './components/firstComponent/firstComponent.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    firstComponentComponent
+    firstComponentComponent,
+    NavbarComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/firstComponent/firstComponent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-firstComponent',
+    // template: '<h1>Hola mundo Angular!</h1><h3>Como estan?</h3><h6>Yo aquí aprendiendo Angular! :)</h6>Los quiero'
+    templateUrl: 'firstComponent.component.html',
+    styleUrls:['firstComponent.component.css']
+})
+
+export class firstComponentComponent{
+    
+}
